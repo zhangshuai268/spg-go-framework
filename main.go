42a3c9c40349a9dc5191702ccba549d4a9f69011
zhangshuai <1779802616@qq.com>
2022-08-15 14:09:05 +0800

    添加更新配置文件指令

--- main.go
+++ main.go
@@ -2,118 +2,118 @@ package main
 
 import (
 	"fmt"
+	"github.com/jessevdk/go-flags"
 	"github.com/zhangshuai268/spg-go-framework/generator"
 	"os"
 	"strconv"
 )
 
+var opts struct {
+	Generator bool `short:"g"`
+	Version   bool `short:"v" long:"version"`
+	Update    bool `short:"u" long:"update"`
+}
+
 func main() {
-	////修改git源
-	//fmt.Println("请输入git仓库地址：")
-	//var url string
-	//_, _ = fmt.Scanln(&url)
-	//for {
-	//	index := strings.Index(url, ".git")
-	//	if index == -1 {
-	//		fmt.Println("请输入正确的git仓库地址：")
-	//		_, _ = fmt.Scanln(&url)
-	//	} else {
-	//		break
-	//	}
-	//}
-	//err := generator.ChangeGit(url)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//初始化go.mod
-	err := generator.RunCommand()
+	getwd, err := os.Getwd()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("初始化go.mod,下载相关依赖")
-	err = generator.FrameworkGenerator()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("生成目录结构成功")
-	err = generator.ConfigGenerator()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("生成配置文件成功")
-	err = generator.StoreGenerator()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("生成数据库目录成功")
-	fmt.Println("请输入开发系统数量：")
-	var n string
-	_, _ = fmt.Scanln(&n)
-	var atoi int
-	for {
-		atoi, err = strconv.Atoi(n)
-		if err == nil {
-			break
-		} else {
-			fmt.Println("请输入数字：")
-			_, _ = fmt.Scanln(&n)
+	_, err = flags.ParseArgs(&opts, os.Args)
+	if opts.Version {
+		fmt.Println("v1.0.6")
+	} else if opts.Update {
+		generator.GenerateModels(getwd+"/config.json", getwd+"/internal/config/config.go")
+	} else if opts.Generator {
+		//初始化go.mod
+		err := generator.RunCommand()
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
-	}
-	for i := 0; i < atoi; i++ {
-		var name, port string
-		fmt.Println("请输入开发系统名称：")
-		_, _ = fmt.Scanln(&name)
-		for {
-			letter := isLetter(name)
-			if letter {
-				break
-			} else {
-				fmt.Println("系统名称不合法：")
-				_, _ = fmt.Scanln(&name)
-			}
+		fmt.Println("初始化go.mod,下载相关依赖")
+		err = generator.FrameworkGenerator()
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
-		fmt.Println("请输入开发系统运行端口号：")
-		_, _ = fmt.Scanln(&port)
+		fmt.Println("生成目录结构成功")
+		err = generator.ConfigGenerator()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("生成配置文件成功")
+		err = generator.StoreGenerator()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("生成数据库目录成功")
+		fmt.Println("请输入开发系统数量：")
+		var n string
+		_, _ = fmt.Scanln(&n)
+		var atoi int
 		for {
-			_, err = strconv.Atoi(port)
+			atoi, err = strconv.Atoi(n)
 			if err == nil {
 				break
 			} else {
-				fmt.Println("请输入正确端口号：")
-				_, _ = fmt.Scanln(&port)
+				fmt.Println("请输入数字：")
+				_, _ = fmt.Scanln(&n)
 			}
 		}
-		//docker文件生成
-		err = generator.DockerGenerator(name, port)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		//swagger文件生成
-		err = generator.SwaggerGenerator(name, port)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		//主要代码目录文件生成
-		err = generator.InternalGenerator(name)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		//启动代码文件生成
-		err = generator.CmdGenerator(name, port)
-		if err != nil {
-			return
+		for i := 0; i < atoi; i++ {
+			var name, port string
+			fmt.Println("请输入开发系统名称：")
+			_, _ = fmt.Scanln(&name)
+			for {
+				letter := isLetter(name)
+				if letter {
+					break
+				} else {
+					fmt.Println("系统名称不合法：")
+					_, _ = fmt.Scanln(&name)
+				}
+			}
+			fmt.Println("请输入开发系统运行端口号：")
+			_, _ = fmt.Scanln(&port)
+			for {
+				_, err = strconv.Atoi(port)
+				if err == nil {
+					break
+				} else {
+					fmt.Println("请输入正确端口号：")
+					_, _ = fmt.Scanln(&port)
+				}
+			}
+			//docker文件生成
+			err = generator.DockerGenerator(name, port)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			//swagger文件生成
+			err = generator.SwaggerGenerator(name, port)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			//主要代码目录文件生成
+			err = generator.InternalGenerator(name)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			//启动代码文件生成
+			err = generator.CmdGenerator(name, port)
+			if err != nil {
+				return
+			}
 		}
 	}
-	_ = os.Remove("./framework_create.exe")
-	_ = os.Remove("./README.md")
+
 }
 
 func isLetter(s string) bool {
