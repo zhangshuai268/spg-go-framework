9035e5c78c64d1373a00e0aa705f7e5ab0d701d2
Zhangshuai <zhangshuai@sc-edu.com>
2022-09-07 18:40:12 +0800

    debug

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	}
 	_, err = flags.ParseArgs(&opts, os.Args)
 	if opts.Version {
-		fmt.Println("v1.1.2")
+		fmt.Println("v1.1.3")
 	} else if opts.Update {
 		generator.GenerateModels(getwd+"/config.json", getwd+"/internal/config/config.go")
 	} else if opts.Generator {
